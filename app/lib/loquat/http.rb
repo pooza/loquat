1f136789fa9cdd67ff39931529777b648a4fa31d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-04-17 15:20:21 +0900

    #1

--- app/lib/loquat/http.rb
+++ app/lib/loquat/http.rb
@@ -3,14 +3,14 @@ module Loquat
     include Package
 
     def get(uri, params = {})
-      path = self.class.create_mock_path(uri, params)
       response = super
-      if params[:mockable] && Environment.development? && Environment.test? && !File.exist?(path)
-        File.write(path, Marshal.dump(response))
+      if params[:mock] && Environment.development? && Environment.test?
+        path = self.class.create_mock_path(uri, params)
+        File.write(path, Marshal.dump(response)) unless File.exist?(path)
       end
       return response
     rescue Ginseng::GatewayError => e
-      if params[:mockable] && Environment.ci? && File.exist?(path)
+      if params[:mock] && Environment.ci? && File.exist?(path)
         mock = Marshal.load(File.read(path)) # rubocop:disable Security/MarshalLoad
         warn "load mock: #{mock.class} #{File.basename(path)}"
         return mock
@@ -19,11 +19,15 @@ module Loquat
     end
 
     def self.create_mock_path(uri, params = {})
-      return File.join(
+      raise "request '#{uri}' not mockable" unless params[:mock]
+      path = File.join(
         Environment.dir,
-        'test/mock/http',
+        'test/mock',
+        params[:mock].to_s.underscore,
         params.merge(uri_path: uri.path).to_json.adler32,
       )
+      FileUtils.mkdir_p(File.dirname(path))
+      return path
     end
   end
 end
