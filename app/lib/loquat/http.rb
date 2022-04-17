b4ac55ce22835dc52426f7e91dd0184ec21a813b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-04-17 14:59:07 +0900

    #1

--- app/lib/loquat/http.rb
+++ app/lib/loquat/http.rb
@@ -12,7 +12,7 @@ module Loquat
     rescue Ginseng::GatewayError => e
       if Environment.ci? && File.exist?(path)
         mock = Marshal.load(File.read(path)) # rubocop:disable Security/MarshalLoad
-        warn "load mock: #{mock.class} #{mock.to_s.adler32}"
+        warn "load mock: #{mock.class} #{File.basename(path)}"
         return mock
       end
       raise Ginseng::GatewayError, e.message, e.backtrace
