5154743305ca9121e7e8a84f604ba7975c24cbba
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-04-19 02:36:04 +0900

    #1

--- app/lib/loquat/http.rb
+++ app/lib/loquat/http.rb
@@ -1,19 +1,5 @@
 module Loquat
   class HTTP < Ginseng::HTTP
     include Package
-
-    def get(uri, options = {})
-      options[:headers] ||= {}
-      options[:headers]['User-Agent'] ||= user_agent
-      repeat(:get, uri = create_uri(uri), start = Time.now) do
-        response = HTTParty.get(uri.normalize, options)
-        log(method: :get, url: uri, status: response.code, start:)
-        raise GatewayError, "Bad response #{response.code}" unless response.code < 400
-        save_mock(response, options)
-        return response
-      end
-    rescue => e
-      return load_mock(error: e, options:)
-    end
   end
 end
