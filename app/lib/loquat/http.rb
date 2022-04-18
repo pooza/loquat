aedecb827e8fc40e3fd28bd334c6f290c166811c
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-04-19 02:14:36 +0900

    #1

--- app/lib/loquat/http.rb
+++ app/lib/loquat/http.rb
@@ -7,13 +7,13 @@ module Loquat
       options[:headers]['User-Agent'] ||= user_agent
       repeat(:get, uri = create_uri(uri), start = Time.now) do
         response = HTTParty.get(uri.normalize, options)
-        log(method: :get, url: uri, status: response.code, start: start)
+        log(method: :get, url: uri, status: response.code, start:)
         raise GatewayError, "Bad response #{response.code}" unless response.code < 400
         save_mock(response, options)
         return response
       end
     rescue => e
-      return load_mock(error: e, options: options)
+      return load_mock(error: e, options:)
     end
   end
 end
